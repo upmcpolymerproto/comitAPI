3783cf83a7161424c1e530c62be069be1e388344
Arin <arinb@upmc.edu>
2017-04-18 14:23:43 -0400

    Updates to getadgroups.js left out on a previous push

--- services/getadgroups.js
+++ services/getadgroups.js
@@ -1,6 +1,8 @@
 'use strict';
 const config = require('../config/config.json');
 const activeDirectory = require('activedirectory');
+const Group = require('../models/group');
+const galaxyLog = require('../helpers/galaxylog');
 const filters = activeDirectory.filters;
 const ad = new activeDirectory(config.activedirectory);
 
@@ -15,7 +17,6 @@ const parseGUID = ( objectGUID ) => {
         objectGUID, value => {
             //Get the least significant byte and convert it to hex.
             s = (value & 0xFF).toString(16);
-
             return value <= 0xF ? ('0' + s) : s;
         }
     );
@@ -31,15 +32,17 @@ const parseGUID = ( objectGUID ) => {
 /**
  * Queries the AD server and returns a subset of groups containing the specified string.
  * @param {String} contains The parameter from the user to search all groups for.
- * @return {Promise} A promise that resolves to a collection of group GUIDs and group names, resolves to a string indicating no groups were found or rejects to an error message.
+ * @return {Promise} A promise that resolves to a collection of Group objects, resolves to a string indicating no groups were found or rejects to an error message.
  */
 const getADGroups =  (contains) => 
     new Promise((resolve, reject) => {
     //specify optional params for ldap query
     var opts = {  
-        filter: "CN=*" + contains + "*",
+        filter: "CN=" + contains + "*",
         //specify which attributes to be returned from the AD server
         attributes: ['objectGUID', 'cn'],
+        sizeLimit: 20,
+        timeLimit: 2,
         //specify custom function (parseGUID) to parse the results of the ldap object(s)
         entryParser: (entry, raw, callback) => {
             //parse and overwrite each raw GUID with the format matching xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
@@ -50,10 +53,15 @@ const getADGroups =  (contains) =>
     ad.findGroups(opts, (err, groups) => {
         //ldap query failed
         if (err) {
-            reject(err.message);
+            reject("Error occurred in getADGroups():" + err.message);
         }
-        //ldap query did not fail but did not return any results
-        resolve(groups);
+        //ldap query succeeded
+        var returnGroups = []
+        for(var i = 0; i< groups.length; i++){
+            returnGroups.push(new Group(groups[i].objectGUID, groups[i].cn,[]));
+        };
+        
+        resolve(returnGroups);
     });
 });
 
@@ -63,19 +71,19 @@ module.exports = (request, response, next) =>  {
     let contains = (request.params.contains != null) ? String(request.params.contains).trim() : '';
     if (contains === '') {
         let error = new Error('Please provide a valid parameter');
-        console.log(error);
+        galaxyLog.logMessage(error);
         response.status(400).send(error.message);
     } else {
         getADGroups(contains).then(groups =>{
             if(!groups || groups.length == 0){
-                console.log("No groups found containing \"" + contains + "\"");
+                galaxyLog.logMessage("No groups found containing \"" + contains + "\"");
                 response.status(200).send("No groups found containing \"" + contains + "\"");
             }else{
-                console.log("Query for groups containing \"" + contains +"\" returned " + groups.length + " group(s).");
-                response.status(200).json(groups);
+                galaxyLog.logMessage("Query for groups containing \"" + contains +"\" returned " + groups.length + " group(s).");
+                response.status(200).send(groups);
             }
         }).catch(err => {
-            console.log('ERROR: ' + err);
+            galaxyLog.logMessage('ERROR: ' + err);
             response.status(400).send(err);
         });
     }
