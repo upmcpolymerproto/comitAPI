23724169e3daca2d1b835cbf449ec641a6077557
Christian Boni <boni1331@gmail.com>
2017-04-25 09:32:22 -0400

    getpermissiontypes changed to have query string parameter

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.post('/login', passport.authenticate('authentication', { session: false }), 
 app.post('/renew', passport.authenticate('renewal', { session: false }), renew);
 app.get('/getadgroups/:startsWith', passport.authenticate('authorization', { session: false }), getadgroups);
 app.get('/getcomponenttags/:contains', passport.authenticate('authorization', { session: false }), getcomponenttags);
-app.get('/getpermissiontypes/', passport.authenticate('authorization', { session: false }), getpermissiontypes);
+app.get('/getpermissiontypes/:system', passport.authenticate('authorization', { session: false }), getpermissiontypes);
 app.post('/gettagsandpermissions', passport.authenticate('authorization', { session: false }), gettagsandpermissionsbygroup);
 app.post('/email', email);
 
